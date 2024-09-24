4a5a0d8f9212b68a8914f43c3fadfd114dc636fb
Jaime Jose Stall Filho <82975356+jaimestall@users.noreply.github.com>
2024-09-24 10:57:52 -0300

    redownload training

--- cartridges/app_training/cartridge/client/default/js/main.js
+++
@@ -1,7 +0,0 @@
-window.jQuery = window.$ = require('jquery');
-var processInclude = require("base/util");
-
-$(document).ready(function () {
-    processInclude(require('../../../../../app_storefront_base/cartridge/client/default/js/main'));
-    processInclude(require('./components/test'));
-});ç
