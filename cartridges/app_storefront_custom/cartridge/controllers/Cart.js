0222efdc84c29b5db189ce0715aeb08edf672acc
Jaime Jose Stall Filho <82975356+jaimestall@users.noreply.github.com>
2024-10-07 10:58:11 -0300

    initial attempt cart email
    The next step will be done after i complete the next module of the course

--- cartridges/app_storefront_custom/cartridge/controllers/Cart.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Cart.js
@@ -1,17 +1,31 @@
 'use strict';
  
 var server = require('server');
+var BasketMgr = require('dw/order/BasketMgr'); // Gerencia o carrinho do usuário
  
 server.extend(module.superModule);
  
 server.append('Show', function (req, res, next) {
+    var currentBasket = BasketMgr.getCurrentOrNewBasket();
     var viewData = res.getViewData();
-    viewData.example = "rendering by viewData";
-    var example2 = 'rendering by res.render()'
+
+    viewData.currentBasket = currentBasket;
     res.setViewData(viewData);
-    res.render('cart/cart', {
-        example2: example2
-    })
+
+    return next();
+});
+
+server.post('AddProduct', function (req, res, next) {
+    var viewData = res.getViewData();
+    var currentBasket = BasketMgr.getCurrentOrNewBasket();
+    var productLineItems = currentBasket.productLineItems;
+    var product = req.form.pid;
+    // var quantity = parseInt(req.form.quantity, 10);
+    // var productToAdd = productLineItems.addProductToBasket(product, quantity);
+    viewData.productLineItems = productLineItems;
+    viewData.product = product;
+    res.setViewData(viewData);
+    
     return next();
 });
 
