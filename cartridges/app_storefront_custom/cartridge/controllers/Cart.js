17e1e049956bb382030e8f4a9094fe63710c584a
Jaime Jose Stall Filho <82975356+jaimestall@users.noreply.github.com>
2024-10-11 11:41:12 -0300

    finished exercise mod 8

--- cartridges/app_storefront_custom/cartridge/controllers/Cart.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Cart.js
@@ -2,7 +2,10 @@
  
 var server = require('server');
 server.extend(module.superModule);
+
 var BasketMgr = require('dw/order/BasketMgr');
+var ProductMgr = require('dw/catalog/ProductMgr');
+
 
 function getCartTotal() {
     var currentCart = BasketMgr.getCurrentBasket();
@@ -15,6 +18,26 @@ function getCartTotal() {
     return 0;
 }
 
+function sendCartEmailNotification(customerEmail, productDetails) {
+    var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers')
+    var emailObj = {
+        to: customerEmail,
+        subject: 'Item Added to Cart',
+        from: 'no-reply@yourstore.com',
+        type: emailHelpers.emailTypes.registration
+    };
+
+    var context = {
+        firstName: productDetails.firstName,
+        productName: productDetails.productName,
+        productPrice: productDetails.productPrice,
+        description: productDetails.description,
+        quantity: productDetails.quantity,
+        image: productDetails.image
+    };
+
+    emailHelpers.send(emailObj, 'cart/cartEmailNotification', context);
+}
  
 server.append('Show', function (req, res, next) {
     var viewData = res.getViewData();
@@ -28,8 +51,36 @@ server.append('Show', function (req, res, next) {
     }
 
     res.setViewData(viewData);
+    return next();
+});
+
+server.append('AddProduct', function (req, res, next) {
+    var viewData = res.getViewData();
+    var currentCustomer = req.currentCustomer.raw;
+    // productId from the URL
+    var productId = req.form.pid;
+
+    if (currentCustomer.authenticated && currentCustomer.profile && productId) {
+        var product = ProductMgr.getProduct(productId);
+        if (product) {
+            var image = product.getImage('medium', 0)
+            if(image) {
+                var productDetails = {
+                    firstName: currentCustomer.profile.firstName,
+                    productName: product.name,
+                    productPrice: product.priceModel.price.value,
+                    description: product.shortDescription,
+                    quantity: res.viewData.quantityTotal,
+                    image: image.httpURL
+                }
+                sendCartEmailNotification(currentCustomer.profile.email, productDetails);
+            }
+        }
+    }
+    
 
 
+    res.setViewData(viewData);
     return next();
 });
 
